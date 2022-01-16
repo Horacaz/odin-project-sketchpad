01af4e8cbb2bb41165fd5bcfc2a6e543c34f9809
Horacaz <xistir97@gmail.com>
2022-01-16 20:25:05 -0300

    testing

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-alert("Hello World!");
 
+const MAIN_CONTAINER = document.querySelector(".main-container");
+
+for (i = 0; i < 256; i++){
+let grid = document.createElement("div");
+
+grid.className = "grid";
+
+MAIN_CONTAINER.appendChild(grid);
+
+}
