8e44b77dc68378957cef854de021586f03950e56
Horacaz <xistir97@gmail.com>
2022-01-18 15:20:28 -0300

    added buttons

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 
 const MAIN_CONTAINER = document.querySelector(".main-container");
+const $gridAmountButton = document.querySelector(".grid-number");
 
-for (i = 0; i < 256; i++) {
-    let grid = document.createElement("div");
+$gridAmountButton.onclick = function () {
+    const input = document.querySelector(".options .number");
+    let gridAmount = input.value;
+    let mainContainer = document.querySelector('.main-container')
+    mainContainer.style.cssText = `grid-template-columns: repeat(${gridAmount}, 1fr);
+grid-template-rows: repeat(${gridAmount}, 1fr)`;
 
-    grid.className = "grid";
+    for (i = 0; i < (gridAmount * gridAmount); i++) {
+        let grid = document.createElement("div");
 
-    MAIN_CONTAINER.appendChild(grid);
+        grid.className = "grid";
+
+        MAIN_CONTAINER.appendChild(grid);
+
+    }
+
+    const $grid = document.querySelectorAll(".grid");
+    $grid.forEach(element => element.onmouseover = function () {
+        element.classList = "black";
+    })
 
 }
 
-
-const $grid = document.querySelectorAll(".grid");
-$grid.forEach(grid => grid.onmouseover = function () {
-    grid.classList="black";
-})
