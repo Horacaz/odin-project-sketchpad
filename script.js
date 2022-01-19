a99beb85a2a3d3755d895583611719b202339390
Horacaz <xistir97@gmail.com>
2022-01-19 10:46:22 -0300

    added clear grid, color and rainbow mode functions

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 
 const MAIN_CONTAINER = document.querySelector(".main-container");
 const $gridAmountButton = document.querySelector(".grid-number");
+let colorInput = document.querySelector(".color");
 
 $gridAmountButton.onclick = function () {
+    
+
     const input = document.querySelector(".options .number");
     let gridAmount = input.value;
     let mainContainer = document.querySelector('.main-container')
     mainContainer.style.cssText = `grid-template-columns: repeat(${gridAmount}, 1fr);
-grid-template-rows: repeat(${gridAmount}, 1fr)`;
+    grid-template-rows: repeat(${gridAmount}, 1fr);`;
+
+    createGrid(gridAmount);
+    const $grid = document.querySelectorAll(".grid");
+    $grid.forEach(element => element.onmouseover = function () {
+        element.style.cssText = `background-color:${colorInput.value};`;
+    })
+
+    let $cleanGrid = document.querySelector(".clean-grid")
+    $cleanGrid.onclick = function (){
+        for (i = 0; i < $grid.length; i++){
+            $grid[i].style.cssText = 'background-color: white';
+        }
+    }
+
+    let $rainbowMode = document.querySelector(".rainbow-mode");
+    $rainbowMode.onclick = function () {
+
+        $grid.forEach(element => element.onmouseover = function () {
+            element.style.cssText = `background-color:#${Math.floor(Math.random() * 16777215).toString(16)};`;
+        })
+
+    }
+
+}
+function createGrid(gridAmount) {
 
     for (i = 0; i < (gridAmount * gridAmount); i++) {
         let grid = document.createElement("div");
@@ -17,11 +45,6 @@ grid-template-rows: repeat(${gridAmount}, 1fr)`;
         MAIN_CONTAINER.appendChild(grid);
 
     }
-
-    const $grid = document.querySelectorAll(".grid");
-    $grid.forEach(element => element.onmouseover = function () {
-        element.classList = "black";
-    })
-
 }
+
 
