0d64b198f7124fc7e5cfff7ee57d513000e5ecf0
Horacaz <xistir97@gmail.com>
2022-01-25 11:34:42 -0300

    first version finished

--- script.js
+++ script.js
@@ -1,50 +1,60 @@
 
-const MAIN_CONTAINER = document.querySelector(".main-container");
+const $mainContainer = document.querySelector(".main-container");
 const $gridAmountButton = document.querySelector(".grid-number");
-let colorInput = document.querySelector(".color");
+const $colorInput = document.querySelector(".color");
+const $cleanButton = document.querySelector(".clean-grid");
+const $rainbowModeButton = document.querySelector(".rainbow-mode");
+createGrid(20);
+
+
 
 $gridAmountButton.onclick = function () {
-    
-
+    resetGrid();
     const input = document.querySelector(".options .number");
     let gridAmount = input.value;
-    let mainContainer = document.querySelector('.main-container')
-    mainContainer.style.cssText = `grid-template-columns: repeat(${gridAmount}, 1fr);
-    grid-template-rows: repeat(${gridAmount}, 1fr);`;
-
     createGrid(gridAmount);
+
     const $grid = document.querySelectorAll(".grid");
     $grid.forEach(element => element.onmouseover = function () {
-        element.style.cssText = `background-color:${colorInput.value};`;
-    })
-
-    let $cleanGrid = document.querySelector(".clean-grid")
-    $cleanGrid.onclick = function (){
-        for (i = 0; i < $grid.length; i++){
-            $grid[i].style.cssText = 'background-color: white';
-        }
-    }
-
-    let $rainbowMode = document.querySelector(".rainbow-mode");
-    $rainbowMode.onclick = function () {
-
-        $grid.forEach(element => element.onmouseover = function () {
-            element.style.cssText = `background-color:#${Math.floor(Math.random() * 16777215).toString(16)};`;
+        element.style.cssText = `background-color:${$colorInput.value};`;
         })
-
-    }
-
 }
+
+function resetGrid(){
+    const $grid = document.querySelectorAll(".grid")
+    for(i = 0; i < $grid.length; i++){
+       document.querySelector(".grid").remove();
+        }
+}
+
+
+$cleanButton.onclick = function(){cleanGrid()};
+
+$rainbowModeButton.onclick = function(){rainbowMode()};
+
+
 function createGrid(gridAmount) {
+    $mainContainer.style.cssText = `grid-template-columns: repeat(${gridAmount}, 1fr);
+    grid-template-rows: repeat(${gridAmount}, 1fr);`;
 
     for (i = 0; i < (gridAmount * gridAmount); i++) {
         let grid = document.createElement("div");
-
         grid.className = "grid";
 
-        MAIN_CONTAINER.appendChild(grid);
+        $mainContainer.appendChild(grid);
 
     }
 }
 
-
+function cleanGrid() {
+    const $grid = document.querySelectorAll(".grid");
+        for (i = 0; i < $grid.length; i++) {
+            $grid[i].style.cssText = 'background-color: white';
+        }
+}
+function rainbowMode(){
+    const $grid = document.querySelectorAll(".grid");
+        $grid.forEach(element => element.onmouseover = function () {
+            element.style.cssText = `background-color:#${Math.floor(Math.random() * 16777215).toString(16)};`;
+        })
+    }
